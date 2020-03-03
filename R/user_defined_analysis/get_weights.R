58f7c78361ea4f7d34e021094df5228cd695ade8
Jan Bařinka <honzabarinka@seznam.cz>
2020-03-03 21:36:43 +0100

    Updating get_weights function and documentation

--- R/user_defined_analysis/get_weights.R
+++ R/user_defined_analysis/get_weights.R
@@ -1,111 +1,91 @@
 #' Weight calculation
-#' 
+#'
 #' Get weights for CNA analysis
-#' 
+#'
+#' @param standard table with 4 compulsory columns: chr = chromosome, ENSG = ensembl gene id, sample = the sample name, alteration = CNA of that gene (+1 copy = 1, +2 = 2, -1 = -1, no CNV = 0, other = NA)
+#' @param count_dir directory with count files
+#' @param dipl_samp character vector with diploid samples
+#' @param metadata same table as the input into RNAseqCNV_wrapper function
 get_weights <- function(standard, count_dir, dipl_samp, metadata) {
-  
-  metadata <- fread(metadata, header = TRUE)
-  
-  # create baseline for normalization
-  base_col <- data.frame(sampleName = dipl_samp)
-  base_matr <- fread(file.path(count_dir, metadata$count[metadata$sample == dipl_samp[1]]), header = FALSE)
-  genes <- pull(base_matr, 1)
-  base_matr = base_matr %>% select(-1)
-  for (i in 2:length(dipl_samp)) {
-    count_s <- fread(file.path(count_dir, metadata$count[metadata$sample == dipl_samp[i]]), header = FALSE)[, 2]
-    base_matr <- cbind(base_matr, count_s)
+
+  #function for geometric
+  gm_mean = function(a){prod(a)^(1/length(a))}
+
+  #read metadata table
+  metadata <- fread(metadata, header = FALSE)
+
+  if (any(duplicated(metadata[, 2]))) {
+    stop("Duplicated samples")
   }
-  base_matr <- base_matr %>% as.data.frame() %>% magrittr::set_rownames(genes)
-  
-  # normalize all samples from the standard
-  count_all <- c()
-  for (i in 1:nrow(standard)) {
-    
-    if (! unique(standard$sample)[i] %in% dipl_samp) {
-  
-      # read count file for a sample from standard
-      count_f_s <- metadata[metadata$sample == unique(standard$sample)[i], "count"]
-      count_s <- fread(file.path(count_dir, count_f_s), header = FALSE) %>% as.data.frame() %>% pull(2)
-      
-      # add it to a baseline matrix
-      final_mat <- cbind(count_s, base_matr)
-      final_col <- rbind(data.frame(sampleName = unique(standard$sample)[i]), base_col)
-      
-      #create DESeqDataSet object
-      ddsCount = DESeqDataSetFromMatrix(countData = final_mat, colData = final_col, design= ~ 1)
-      print("Loading count files is done!")
-      
-      #normalize
-      ddsCount <- estimateSizeFactors(ddsCount)
-      count_norm <- counts(ddsCount, normalized = TRUE)
-      
-      if (!is.null(count_all)) {
-        count_all <- cbind(count_all, count_norm[, 1])
-        colnames(count_all)[ncol(count_all)] <- as.character(final_col$sampleName[1])
-      } else {
-        count_all <- data.frame(count_norm)
-        colnames(count_all) <- as.character(final_col$sampleName)
-      }
-    }
+
+  #read in all count files
+  count_table <- read.table(file = paste0(count_dir, metadata[1, 2]), header = FALSE, row.names = 1, stringsAsFactors = FALSE)
+  for (i in 2:nrow(metadata)) {
+    count_table <- cbind(count_table, read.table(file = paste0(count_dir, metadata[i, 2]), header = FALSE, row.names = 1, stringsAsFactors = FALSE))
   }
-  
-  #annotate the normalized counts with chr and arm
-  count_all_an <- count_all %>% mutate(ENSG = row.names(.)) %>% merge(refDataExp) %>% merge(centr_ref) %>% mutate(arm = ifelse(start < cstart, "p", ifelse(start > cend, "q", "centr"))) %>%
-    select(-start, -end, -cstart, -cend)
-  
-  #calculate median of expression 
-  count_dipl <- count_all_an[, which(colnames(count_all_an) %in% c(dipl_samp, "chr", "ENSG", "arm"))]
-  med = apply(select(count_dipl, -chr, -ENSG), 1, median)
-  #establish diploid expression baseline per chromosome
-  ref_dipl = cbind(select(count_dipl, chr, arm, ENSG), median = as.numeric(med)) %>% group_by(chr, arm) %>% summarise(sum_chr_dipl = sum(median))
-  
-  #adjust non-diploid samples
-  count_wide <- count_long %>% spread(key = sample, value = norm_exp)
-  #choose non-diploid samples
-  count_wide_non_dipl <- count_wide[, which(!colnames(count_wide) %in% dipl_samp)]
-  #choose which columns to adjust
-  to_adj <- which(! colnames(count_wide_non_dipl) %in% c("chr", "arm", "ENSG"))
-  count_adj <- count_wide_non_dipl
-  for (i in to_adj) {
-    exp_sample <- count_wide_non_dipl[, c("chr", "arm", colnames(count_wide_non_dipl)[i])] 
-    colnames(exp_sample)[3] <- "exp"
-    #get sums of counts per chromosome and calculate the sife factor compared to diploid samples
-    dipl <- filter(standard, sample == colnames(count_wide_non_dipl)[i], alteration == "0") %>% select(chr, arm)
-    chrom_sum_samp_dipl <- exp_sample %>% group_by(chr, arm) %>% summarise(sum_chr_nd = sum(exp, na.rm = TRUE)) %>% ungroup() %>% merge(ref_dipl, by = c("chr", "arm")) %>% mutate(sizefac = sum_chr_dipl / sum_chr_nd) %>% 
-      right_join(dipl)
-    #calculate final sample size factor
-    final_sf <- median(chrom_sum_samp_dipl$sizefac)
-    #adjust the sample
-    count_adj[, i] <- count_adj[, i] * final_sf
+  colnames(count_table) <- pull(metadata, 1)
+  count_table <- as.data.frame(count_table)
+
+  ####filtering of samples###
+  count_filt = as.data.frame(count_table) %>% mutate(keep_gene = apply(., MARGIN = 1, FUN = function(x) sum(x > 2) > (length(x) * 0.8)), ENSG = rownames(.), id = row_number()) %>% filter(keep_gene == TRUE) %>%
+    select(-keep_gene, -id, -ENSG)
+
+  ###normalize samples and calculate vst####
+  count_norm <- count_filt
+  count_dipl <- count_filt[colnames(count_filt) %in% dipl_samp]
+  non_dipl <- pull(metadata, 1)[!pull(metadata, 1) %in% dipl_samp]
+  for (i in 1:ncol(count_filt)) {
+    to_trans <- count_filt %>% select(i) %>% bind_cols(count_dipl)
+    count_matr <- as.matrix(to_trans)
+    count_col <- as.data.frame(colnames(to_trans))
+
+    dds <- DESeqDataSetFromMatrix(colData = count_col, countData = count_matr, design= ~ 1)
+    dds_vst <- varianceStabilizingTransformation(dds, blind=T, fitType='local')
+    count_norm[, i] <- assay(dds_vst)[, 1]
   }
-  
-  #merge dipl and non-dipl
-  count_all_adj <- merge(count_adj, count_dipl, by = c("chr", "arm", "ENSG"))
-  
+
+  #annotate the normalized counts with chr
+  count_norm_an <-  count_norm %>% mutate(ENSG = row.names(.)) %>% merge(refDataExp)
+
+  #calculate pseudo reference in diploid samples for second normalization
+  count_dipl_norm <-  count_norm_an[, which(colnames( count_norm_an) %in% c(dipl_samp, "chr", "ENSG"))]
+  pseudo_ref_norm <- apply(X = select(count_dipl_norm, -chr, - ENSG), MARGIN = 1, function(x) gm_mean(x))
+  ref_dipl = cbind(select(count_dipl_norm, chr, ENSG), pseudo_ref = as.numeric(pseudo_ref_norm)) %>% filter(pseudo_ref != 0)
+
+  #second normalization, especially for samples with high numbers of CNA
+  count_norm_an_cor <- count_norm_an
+  for (i in non_dipl) {
+    sample_vst <-  count_norm_an[, c(i, "chr", "ENSG")]
+    colnames(sample_vst)[1] <- "count"
+    size_fac = sample_vst %>% right_join(ref_dipl, by = c("ENSG", "chr")) %>% left_join(filter(standard, sample == i), by = c("ENSG", "chr")) %>% filter(alteration == 0) %>% mutate(size_facs = count/pseudo_ref) %>% summarise(median(size_facs))
+    count_norm_an_cor[, i] <- count_norm_an_cor[, i] * size_fac
+  }
+
   #annotate the data with alteration information
-  count_all_adj_an <- gather(count_all_adj, key = "sample", value = "exp", which(!colnames(count_all_adj) %in% c("chr", "arm", "ENSG"))) %>% right_join(select(standard, sample, chr, arm, alteration))
- 
+  count_final <- gather(count_norm_an_cor, key = "sample", value = "exp", which(!colnames(count_norm_an_cor) %in% c("chr", "ENSG"))) %>% left_join(select(standard, sample, ENSG, chr, alteration), by = c("ENSG", "sample", "chr"))
+
   #get per-gene CNA expression correlation
-  genes <- unique(count_all_adj_an$ENSG)
+  genes <- unique(count_final$ENSG)
   gene_cor <- c()
-  for (i in 1:length(genes)) {
-    gene_data <- count_all_adj_an %>% filter(ENSG == genes[i])
+  for (i in 1:10) {
+    gene_data <- count_final %>% filter(ENSG == genes[i])
     cor <- cor.test(x = gene_data$exp, y = as.numeric(gene_data$alteration))
-    
+
     if (!is.null(gene_cor)) {
       gene_cor <- rbind(gene_cor, data.frame(ENSG = genes[i], pearson_r = cor$estimate, p = cor$p.value))
     } else {
       gene_cor <- data.frame(ENSG = genes[i], pearson_r = cor$estimate, p = cor$p.value)
     }
   }
-  
-  #get per-gene variance
-  count_var <- count_all_adj %>% mutate(var = apply(select(., -ENSG, -chr, -arm), 1, function(x) var(x, na.rm = TRUE))) %>% arrange(var)
-  
+
+  #get per-gene variance and depth
+  var <- apply(select(count_dipl_norm, -chr, -ENSG), 1, var)
+  depth <- apply(select(count_dipl_norm, -chr, -ENSG), 1, mean)
+  var_depth_table <- data.frame(ENSG = count_dipl_norm$ENSG, var = var, depth = depth, chr = count_dipl_norm$chr)
+
   #calculate weights
-  weight_table <- select(count_var, ENSG, var) %>% right_join(gene_cor, by = "ENSG") %>% mutate(pearson_r = ifelse(pearson_r < 0.05, 0, pearson_r), pearson_r_q = pearson_r^2,   var_div_q = 1/var^2, weight = pearson_r_q*var_div_q) %>%
-    arrange(desc(weight)) %>% select(ENSG, weight)
-  
+  wgt_tab <- var_depth_table %>% right_join(gene_cor, by = "ENSG") %>% mutate(weight = pearson_r^2*(1/var^2)*depth^2) %>% mutate(chromosome_name = chr) %>%
+    arrange(desc(weight)) %>% dplyr::select(ENSG, weight, chromosome_name)
+
   return(weight_table)
 }
- 
